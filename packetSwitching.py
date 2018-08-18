e757cbd3c7c4bfe47bfee9769eefa49ab72d4d72
Matthew Ritter <mritter2283@gmail.com>
2018-08-18 09:39:58 -0400

    Added README file and packetSwitching.py file

---
+++ packetSwitching.py
@@ -0,0 +1,95 @@
+'''
+Matthew Ritter
+CS4121
+Assignment 1
+Due Date 08/23/18
+
+MATPLOTLIB IS NEEDED TO RUN THIS PROGRAM.
+IT IS USED TO SHOW THE DATA GRAPHICALY.
+pip install matplotib
+'''
+
+# import math and pyplot
+import math
+from matplotlib import pyplot as plt
+# x and y points for zero bit overhead
+overhead_0_x = []
+overhead_0_y = []
+
+# x and y points for ten bit overhead
+overhead_10_x = []
+overhead_10_y = []
+
+# x and y points for twenty bit overhead
+overhead_20_x = []
+overhead_20_y = []
+
+
+# Method to create and plot each line with correct amount of overhead
+def packetSwitching(listx, listy, overhead):
+    # This problem only uses kb which is 10^3 we easly make add mb or gb
+    kb = math.pow(10, 3)
+    # Only needed if the problem changes
+    #mb = math.pow(10,6)
+    #gb = math.pow(10,9)
+
+    # Bandwith rate of 4kbps
+    R = 4 * kb
+
+    # Message of size 16kb
+    L = 16 * kb
+
+    # Number of Router/Switches
+    numberOfRouters = 2
+
+    # Packets vary from (largest) 4kb to 2kb to 1kb ... to (smallest)125 bits
+    largestPacket = (4 * kb)
+    smallestPacket = 125
+
+    pointsArray = []
+    print("=======================")
+    print("With overhead: ", overhead, " bits")
+    # Start from largest packet
+    while largestPacket >= smallestPacket:
+        # Segmentation we need to divied the total message up
+        numberOfPackets = L / largestPacket
+        # The number of packets and the total number of routers will give us the number we need to add to the delay
+        numberOfTransmissions = numberOfPackets + numberOfRouters
+        # The delay is the packetsize along with the overhead (IP Addresses) divided by the bandwith (R)
+        delay = (largestPacket + overhead) / R
+        # With all transmissions plus the extra overhead and the extra time due to the number of routers we get end to end delay
+        endToEnd = numberOfTransmissions * delay
+        # We need an array with x coordinates and one with y coordinates to use matplotlib to make the line graph
+        listx.append(largestPacket)
+        listy.append(endToEnd)
+
+        # Terminal view of each point
+        print((largestPacket + overhead),
+              "bits will have and end-to-end delay of", endToEnd)
+
+        # The size of each packet is cut in half
+        largestPacket = largestPacket / 2
+
+    # After the loop give pyplot each array of x's and y's, label for the legend, and markers for each point on the line
+    plt.plot(listx, listy, label="Overhead= "+str(overhead)+"b", marker='o')
+    print("=======================")
+
+
+# Call packetSwitching() with each overhead 0 bits, 10 bits, 20 bits
+packetSwitching(overhead_0_x, overhead_0_y, 0)
+packetSwitching(overhead_10_x, overhead_10_y, 10)
+packetSwitching(overhead_20_x, overhead_20_y, 20)
+
+# Use pyplot to show and clean up data
+plt.xlabel("Packet Size In Bits")
+plt.ylabel("End to End In Seconds")
+plt.title("Packet Switching")
+# Invert the x-axis, make the graph easier to read
+plt.gca().invert_xaxis()
+# Set better limites to the line graph, makes it look better
+plt.gca().set_ylim([4, 6.5])
+plt.gca().set_xlim([4000, 0])
+# Create a legend to under stand each line
+plt.legend()
+# Finally, tell pyplot to show the graph
+plt.show()
