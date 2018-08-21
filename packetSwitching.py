e015cdfea7976953befc221c430bcb97cd471ce7
Matthew Ritter <mritter2283@gmail.com>
2018-08-21 13:37:37 -0400

    Cleaned up the code try to simplify things

--- packetSwitching.py
+++ packetSwitching.py
@@ -13,21 +13,8 @@ pip install matplotib
 import math
 from matplotlib import pyplot as plt
 
-# x and y points for zero bit overhead
-overhead_0_x = []
-overhead_0_y = []
-
-# x and y points for ten bit overhead
-overhead_10_x = []
-overhead_10_y = []
-
-# x and y points for twenty bit overhead
-overhead_20_x = []
-overhead_20_y = []
 
 # Need to call for both graphs
-
-
 def makeGraph():
     # Use pyplot to show and clean up data
     plt.xlabel("Packet Size In Bits")
@@ -41,7 +28,11 @@ def makeGraph():
 # Method to create and plot each line with correct amount of overhead
 
 
-def packetSwitching(listx, listy, overhead):
+def packetSwitching(overhead=0):
+
+    listx = []
+    listy = []
+
     # This problem only uses kb which is 10^3 we easly make add mb or gb
     kb = math.pow(10, 3)
     # Only needed if the problem changes
@@ -54,23 +45,24 @@ def packetSwitching(listx, listy, overhead):
     # Message of size 16kb
     L = 16 * kb
 
-    # Number of Router/Switches
+    # Number of Routers/Switches
     numberOfRouters = 2
 
     # Packets vary from (largest) 4kb to 2kb to 1kb ... to (smallest)125 bits
     largestPacket = (4 * kb)
     smallestPacket = 125
 
-    pointsArray = []
+    # Terminal view info
     print("=======================")
     print("With overhead: ", overhead, " bits")
-    # Start from largest packet
+
+    # Loop to simulate packet switching segmintation
     while largestPacket >= smallestPacket:
-        # Segmentation we need to divied the total message up
+        # Segmentation - we need to divied the total message up
         numberOfPackets = L / largestPacket
         # The number of packets and the total number of routers will give us the number we need to add to the delay
         numberOfTransmissions = numberOfPackets + numberOfRouters
-        # The delay is the packetsize along with the overhead (IP Addresses) divided by the bandwith (R)
+        # The delay is the packetsize along with the overhead infromation (e.g. IP Addresses) divided by the bandwith (R)
         delay = (largestPacket + overhead) / R
         # With all transmissions plus the extra overhead and the extra time due to the number of routers we get end to end delay
         endToEnd = numberOfTransmissions * delay
@@ -82,7 +74,7 @@ def packetSwitching(listx, listy, overhead):
         print((largestPacket + overhead),
               "bits will have and end-to-end delay of", endToEnd)
 
-        # The size of each packet is cut in half
+        # The size of each packet is cut in half and pushed back through the loop
         largestPacket = largestPacket / 2
 
     # After the loop give pyplot each array of x's and y's, label for the legend, and markers for each point on the line
@@ -102,13 +94,14 @@ def packetSwitching(listx, listy, overhead):
     # Give the second graph an x axis scaled logarithmically
     plt.xscale('log')
 
+    # Terminal view delimiter
     print("=======================")
 
 
 # Call packetSwitching() with each overhead 0 bits, 10 bits, 20 bits
-packetSwitching(overhead_0_x, overhead_0_y, 0)
-packetSwitching(overhead_10_x, overhead_10_y, 10)
-packetSwitching(overhead_20_x, overhead_20_y, 20)
+packetSwitching()
+packetSwitching(10)
+packetSwitching(20)
 
 # Finally, tell pyplot to show both graphs
 plt.show()
